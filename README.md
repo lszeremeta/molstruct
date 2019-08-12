7982f5ea02bcf4e662788574c11cd109af75c72f
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2019-08-13 00:05:31 +0200

    Add new columns

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # molstruct
 
 Converts chemical molecule data [Comma Separated Values (CSV)](https://en.wikipedia.org/wiki/Comma-separated_values) files to structured data formats - [JSON-LD](https://json-ld.org/), [RDFa](http://rdfa.info/) and [Microdata](https://schema.org/docs/gs.html). Supported
-CSV columns: `id`, `name`, `inchikey`, `inchi`, `smiles`, `url`, `iupacname`, `molecularformula` and `description`. Written in [Python](https://www.python.org/) 3. Works from CLI.
+CSV columns: `identifier`, `name`, `inChIKey`, `inChI`, `smiles`, `url`, `iupacName`, `molecularFormula`, `molecularWeight`, `monoisotopicMolecularWeight`, `description`, `disambiguatingDescription`, `image`, `additionalType`, `alternateName` and `sameAs`.  Written in [Python](https://www.python.org/) 3. Works from CLI.
 
 ## What are structured data?
 Structured data are additional data placed on websites. They are not visible to ordinary internet users, but can be easily processed by machines. There are 3 formats that we can use to save structured data - [JSON-LD](https://json-ld.org/), [RDFa](http://rdfa.info/) and [Microdata](https://www.w3.org/TR/microdata/). Molstruct supports them all and use [MolecularEntitly](https://bioschemas.org/types/MolecularEntity/) type.
@@ -12,10 +12,13 @@ There are many possibilities. The easiest way is to download a CSV file from one
 ## Usage
 
 ```shell
-usage: molstruct.py [-h] (-jh | -j | -r | -m) [-i ID] [-n NAME]
+usage: molstruct.py [-h] (-jh | -j | -r | -m) [-i IDENTIFIER] [-n NAME]
                     [-ink INCHIKEY] [-in INCHI] [-s SMILES] [-u URL]
-                    [-iu IUPACNAME] [-f MOLECULARFORMULA] [-d DESCRIPTION]
-                    [-c] [-l LIMIT]
+                    [-iu IUPACNAME] [-f MOLECULARFORMULA] [-w MOLECULARWEIGHT]
+                    [-mw MONOISOTOPICMOLECULARWEIGHT] [-d DESCRIPTION]
+                    [-dd DISAMBIGUATINGDESCRIPTION] [-img IMAGE]
+                    [-at ADDITIONALTYPE] [-an ALTERNATENAME] [-sa SAMEAS] [-c]
+                    [-l LIMIT]
                     file
 ```
 
@@ -33,22 +36,43 @@ usage: molstruct.py [-h] (-jh | -j | -r | -m) [-i ID] [-n NAME]
   -j, --jsonld          JSON-LD output
   -r, --rdfa            RDFa output
   -m, --microdata       Microdata output
-  -i ID, --id ID        identifier column name (id by default)
-  -n NAME, --name NAME  name column name (name by default)
-  -ink INCHIKEY, --inchikey INCHIKEY
-                        inchikey column name (inchikey by default)
-  -in INCHI, --inchi INCHI
-                        inchi column name (inchi by default)
+  -i IDENTIFIER, --identifier IDENTIFIER
+                        identifier column name (identifier by default), Text
+  -n NAME, --name NAME  name column name (name by default), Text
+  -ink INCHIKEY, --inChIKey INCHIKEY
+                        inChIKey column name (inChIKey by default), Text
+  -in INCHI, --inChI INCHI
+                        inChI column name (inChI by default), Text
   -s SMILES, --smiles SMILES
-                        smiles column name (smiles by default)
-  -u URL, --url URL     url column name (url by default)
-  -iu IUPACNAME, --iupacname IUPACNAME
-                        iupacname column name (iupacname by default)
-  -f MOLECULARFORMULA, --molecularformula MOLECULARFORMULA
-                        molecularformula column name (molecularformula by
-                        default)
+                        smiles column name (smiles by default), Text
+  -u URL, --url URL     url column name (url by default), URL type
+  -iu IUPACNAME, --iupacName IUPACNAME
+                        iupacName column name (iupacName by default), Text
+  -f MOLECULARFORMULA, --molecularFormula MOLECULARFORMULA
+                        molecularFormula column name (molecularFormula by
+                        default), Text
+  -w MOLECULARWEIGHT, --molecularWeight MOLECULARWEIGHT
+                        molecularWeight column name (molecularWeight by
+                        default), Mass e.g. 0.01 mg)
+  -mw MONOISOTOPICMOLECULARWEIGHT, --monoisotopicMolecularWeight MONOISOTOPICMOLECULARWEIGHT
+                        monoisotopicMolecularWeight column name
+                        (monoisotopicMolecularWeight by default), Mass e.g.
+                        0.01 mg
   -d DESCRIPTION, --description DESCRIPTION
-                        description column name (description by default)
+                        description column name (description by default), Text
+  -dd DISAMBIGUATINGDESCRIPTION, --disambiguatingDescription DISAMBIGUATINGDESCRIPTION
+                        disambiguatingDescription column name
+                        (disambiguatingDescription by default), Text
+  -img IMAGE, --image IMAGE
+                        image column name (image by default), URL
+  -at ADDITIONALTYPE, --additionalType ADDITIONALTYPE
+                        additionalType column name (additionalType by
+                        default), URL
+  -an ALTERNATENAME, --alternateName ALTERNATENAME
+                        alternateName column name (alternateName by default),
+                        Text
+  -sa SAMEAS, --sameAs SAMEAS
+                        sameAs column name (sameAs by default), URL
   -c, --columns         Use only columns with renamed names
   -l LIMIT, --limit LIMIT
                         Maximum number of results
--- molstruct.py
+++ molstruct.py
@@ -14,8 +14,8 @@ def create_jsonld_output(reader):
         out_str += '  {\n'
         out_str += '  "@id" : "http://example.org/me' + str(i) + '",\n'
         out_str += '  "@type" : "https://schema.org/MolecularEntity",\n'
-        if row.get(ID):
-            out_str += '  "identifier" : ' + json.dumps(row.get(ID)) + ',\n'
+        if row.get(IDENTIFIER):
+            out_str += '  "identifier" : ' + json.dumps(row.get(IDENTIFIER)) + ',\n'
         if row.get(NAME):
             out_str += '  "name" : ' + json.dumps(row.get(NAME)) + ',\n'
         if row.get(INCHIKEY):
@@ -26,12 +26,24 @@ def create_jsonld_output(reader):
             out_str += '  "smiles" : ' + json.dumps(row.get(SMILES)) + ',\n'
         if row.get(URL):
             out_str += '  "url" : ' + json.dumps(row.get(URL)) + ',\n'
-        if row.get(IUPACNAME):
-            out_str += '  "iupacName" : ' + json.dumps(row.get(IUPACNAME)) + ',\n'
-        if row.get(MOLECULARFORMULA):
-            out_str += '  "molecularFormula" : ' + json.dumps(row.get(MOLECULARFORMULA)) + ',\n'
+        if row.get(IUPAC_NAME):
+            out_str += '  "iupacName" : ' + json.dumps(row.get(IUPAC_NAME)) + ',\n'
+        if row.get(MOLECULAR_FORMULA):
+            out_str += '  "molecularFormula" : ' + json.dumps(row.get(MOLECULAR_FORMULA)) + ',\n'
+        if row.get(MOLECULAR_WEIGHT):
+            out_str += '  "molecularWeight" : ' + json.dumps(row.get(MOLECULAR_WEIGHT)) + ',\n'
+        if row.get(MONOISOTOPIC_MOLECULAR_WEIGHT):
+            out_str += '  "monoisotopicMolecularWeight" : ' + json.dumps(row.get(MONOISOTOPIC_MOLECULAR_WEIGHT)) + ',\n'
         if row.get(DESCRIPTION):
             out_str += '  "description" : ' + json.dumps(row.get(DESCRIPTION)) + ',\n'
+        if row.get(DISAMBIGUATING_DESCRIPTION):
+            out_str += '  "disambiguatingDescription" : ' + json.dumps(row.get(DISAMBIGUATING_DESCRIPTION)) + ',\n'
+        if row.get(ADDITIONAL_TYPE):
+            out_str += '  "additionalType" : ' + json.dumps(row.get(ADDITIONAL_TYPE)) + ',\n'
+        if row.get(ALTERNATE_NAME):
+            out_str += '  "alternateName" : ' + json.dumps(row.get(ALTERNATE_NAME)) + ',\n'
+        if row.get(SAME_AS):
+            out_str += '  "sameAs" : ' + json.dumps(row.get(SAME_AS)) + ',\n'
         out_str = out_str[:-2] + '\n'
         out_str += '  },'
 
@@ -67,8 +79,29 @@ def create_jsonld_output(reader):
     "molecularFormula" : {
       "@id" : "https://schema.org/molecularFormula"
     },
+    "molecularWeight" : {
+      "@id" : "https://schema.org/molecularWeight"
+    },
+    "monoisotopicMolecularWeight" : {
+      "@id" : "https://schema.org/monoisotopicMolecularWeight"
+    },
     "description" : {
       "@id" : "https://schema.org/description"
+    },
+    "disambiguatingDescription" : {
+      "@id" : "https://schema.org/disambiguatingDescription"
+    },
+    "image" : {
+      "@id" : "https://schema.org/image"
+    },
+    "additionalType" : {
+      "@id" : "https://schema.org/additionalType"
+    },
+    "alternateName" : {
+      "@id" : "https://schema.org/alternateName"
+    },
+    "sameAs" : {
+      "@id" : "https://schema.org/sameAs"
     },
     "schema" : "https://schema.org/",
     "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -86,8 +119,8 @@ def create_rdfa_output(reader):
   <body vocab="http://schema.org/">''')
     for row in reader:
         print('    <div typeof="schema:MolecularEntity" about="http://example.org/me' + str(i) + '">')
-        if row.get(ID):
-            print('      <div property="schema:identifier">' + html.escape(row.get(ID)) + '</div>')
+        if row.get(IDENTIFIER):
+            print('      <div property="schema:identifier">' + html.escape(row.get(IDENTIFIER)) + '</div>')
         if row.get(NAME):
             print('      <div property="schema:name">' + html.escape(row.get(NAME)) + '</div>')
         if row.get(INCHIKEY):
@@ -98,14 +131,37 @@ def create_rdfa_output(reader):
             print('      <div property="schema:smiles">' + html.escape(row.get(SMILES)) + '</div>')
         if row.get(URL):
             print(
-                    '      <a rel="schema:url" href="' + html.escape(row.get(URL)) + '">' + html.escape(
-                row.get(URL)) + '</a>')
-        if row.get(IUPACNAME):
-            print('      <div property="schema:iupacName">' + html.escape(row.get(IUPACNAME)) + '</div>')
-        if row.get(MOLECULARFORMULA):
-            print('      <div property="schema:molecularFormula">' + html.escape(row.get(MOLECULARFORMULA)) + '</div>')
+                '      <a rel="schema:url" href="' + html.escape(row.get(URL)) + '">' + html.escape(
+                    row.get(URL)) + '</a>')
+        if row.get(IUPAC_NAME):
+            print('      <div property="schema:iupacName">' + html.escape(row.get(IUPAC_NAME)) + '</div>')
+        if row.get(MOLECULAR_FORMULA):
+            print('      <div property="schema:molecularFormula">' + html.escape(row.get(MOLECULAR_FORMULA)) + '</div>')
+        if row.get(MOLECULAR_WEIGHT):
+            print('      <div property="schema:molecularWeight">' + html.escape(row.get(MOLECULAR_WEIGHT)) + '</div>')
+        if row.get(MONOISOTOPIC_MOLECULAR_WEIGHT):
+            print('      <div property="schema:monoisotopicMolecularWeight">' + html.escape(
+                row.get(MONOISOTOPIC_MOLECULAR_WEIGHT)) + '</div>')
         if row.get(DESCRIPTION):
             print('      <div property="schema:description">' + html.escape(row.get(DESCRIPTION)) + '</div>')
+        if row.get(DISAMBIGUATING_DESCRIPTION):
+            print('      <div property="schema:disambiguatingDescription">' + html.escape(
+                row.get(DISAMBIGUATING_DESCRIPTION)) + '</div>')
+        if row.get(IMAGE):
+            print(
+                '      <a rel="schema:image" href="' + html.escape(row.get(IMAGE)) + '">' + html.escape(
+                    row.get(IMAGE)) + '</a>')
+        if row.get(ADDITIONAL_TYPE):
+            print(
+                '      <a rel="schema:additionalType" href="' + html.escape(
+                    row.get(ADDITIONAL_TYPE)) + '">' + html.escape(
+                    row.get(ADDITIONAL_TYPE)) + '</a>')
+        if row.get(ALTERNATE_NAME):
+            print('      <div property="schema:alternateName">' + html.escape(row.get(ALTERNATE_NAME)) + '</div>')
+        if row.get(SAME_AS):
+            print(
+                '      <a rel="schema:sameAs" href="' + html.escape(row.get(SAME_AS)) + '">' + html.escape(
+                    row.get(SAME_AS)) + '</a>')
         print('    </div>')
 
         if i == args.limit:
@@ -127,8 +183,8 @@ def create_microdata_output(reader):
     for row in reader:
         print('    <div itemscope itemtype="http://schema.org/MolecularEntity" itemid="http://example.org/me' + str(
             i) + '">')
-        if row.get(ID):
-            print('      <div itemprop="identifier">' + html.escape(row.get(ID)) + '</div>')
+        if row.get(IDENTIFIER):
+            print('      <div itemprop="identifier">' + html.escape(row.get(IDENTIFIER)) + '</div>')
         if row.get(NAME):
             print('      <div itemprop="name">' + html.escape(row.get(NAME)) + '</div>')
         if row.get(INCHIKEY):
@@ -140,12 +196,32 @@ def create_microdata_output(reader):
         if row.get(URL):
             print('      <a href="' + html.escape(row.get(URL)) + '" itemprop="url">' + html.escape(
                 row.get(URL)) + '</a>')
-        if row.get(IUPACNAME):
-            print('      <div itemprop="iupacName">' + html.escape(row.get(IUPACNAME)) + '</div>')
-        if row.get(MOLECULARFORMULA):
-            print('      <div itemprop="molecularFormula">' + html.escape(row.get(MOLECULARFORMULA)) + '</div>')
+        if row.get(IUPAC_NAME):
+            print('      <div itemprop="iupacName">' + html.escape(row.get(IUPAC_NAME)) + '</div>')
+        if row.get(MOLECULAR_FORMULA):
+            print('      <div itemprop="molecularFormula">' + html.escape(row.get(MOLECULAR_FORMULA)) + '</div>')
+        if row.get(MOLECULAR_WEIGHT):
+            print('      <div itemprop="molecularWeight">' + html.escape(row.get(MOLECULAR_WEIGHT)) + '</div>')
+        if row.get(MONOISOTOPIC_MOLECULAR_WEIGHT):
+            print('      <div itemprop="monoisotopicMolecularWeight">' + html.escape(
+                row.get(MONOISOTOPIC_MOLECULAR_WEIGHT)) + '</div>')
         if row.get(DESCRIPTION):
             print('      <div itemprop="description">' + html.escape(row.get(DESCRIPTION)) + '</div>')
+        if row.get(DISAMBIGUATING_DESCRIPTION):
+            print('      <div itemprop="disambiguatingDescription">' + html.escape(
+                row.get(DISAMBIGUATING_DESCRIPTION)) + '</div>')
+        if row.get(IMAGE):
+            print('      <a href="' + html.escape(row.get(IMAGE)) + '" itemprop="image">' + html.escape(
+                row.get(IMAGE)) + '</a>')
+        if row.get(ADDITIONAL_TYPE):
+            print('      <a href="' + html.escape(
+                row.get(ADDITIONAL_TYPE)) + '" itemprop="additionalType">' + html.escape(
+                row.get(ADDITIONAL_TYPE)) + '</a>')
+        if row.get(ALTERNATE_NAME):
+            print('      <div itemprop="alternateName">' + html.escape(row.get(ALTERNATE_NAME)) + '</div>')
+        if row.get(SAME_AS):
+            print('      <a href="' + html.escape(row.get(ADDITIONAL_TYPE)) + '" itemprop="sameAs">' + html.escape(
+                row.get(SAME_AS)) + '</a>')
         print('    </div>')
 
         if i == args.limit:
@@ -157,16 +233,25 @@ def create_microdata_output(reader):
 
 
 # set column names
-ID = 'id'
+IDENTIFIER = 'identifier'
 NAME = 'name'
-INCHIKEY = 'inchikey'
-INCHI = 'inchi'
+INCHIKEY = 'inChIKey'
+INCHI = 'inChI'
 SMILES = 'smiles'
 URL = 'url'
-IUPACNAME = 'iupacname'
-MOLECULARFORMULA = 'molecularformula'
+IUPAC_NAME = 'iupacName'
+MOLECULAR_FORMULA = 'molecularFormula'
+MOLECULAR_WEIGHT = 'molecularWeight'
+MONOISOTOPIC_MOLECULAR_WEIGHT = 'monoisotopicMolecularWeight'
 DESCRIPTION = 'description'
-DEFAULT_COLUMN_NAMES = [ID, NAME, INCHIKEY, INCHI, SMILES, URL, IUPACNAME, MOLECULARFORMULA, DESCRIPTION]
+DISAMBIGUATING_DESCRIPTION = 'disambiguatingDescription'
+IMAGE = 'image'
+ADDITIONAL_TYPE = 'additionalType'
+ALTERNATE_NAME = 'alternateName'
+SAME_AS = 'sameAs'
+DEFAULT_COLUMN_NAMES = [IDENTIFIER, NAME, INCHIKEY, INCHI, SMILES, URL, IUPAC_NAME, MOLECULAR_FORMULA, MOLECULAR_WEIGHT,
+                        MONOISOTOPIC_MOLECULAR_WEIGHT, DESCRIPTION, DISAMBIGUATING_DESCRIPTION, IMAGE, ADDITIONAL_TYPE,
+                        ALTERNATE_NAME, SAME_AS]
 
 parser = argparse.ArgumentParser(
     description='Converts chemical molecule data CSV files to Structured Data formats - JSON-LD, RDFa and Microdata. '
@@ -185,17 +270,31 @@ formats_group.add_argument("-m", "--microdata", help="Microdata output",
                            action="store_true")
 
 # optional column names changes
-parser.add_argument("-i", "--id", type=str, help="identifier column name (" + ID + " by default)")
-parser.add_argument("-n", "--name", type=str, help="name column name (" + NAME + " by default)")
-parser.add_argument("-ink", "--inchikey", type=str, help="inchikey column name (" + INCHIKEY + " by default)")
-parser.add_argument("-in", "--inchi", type=str, help="inchi column name (" + INCHI + " by default)")
-parser.add_argument("-s", "--smiles", type=str, help="smiles column name (" + SMILES + " by default)")
-parser.add_argument("-u", "--url", type=str, help="url column name (" + URL + " by default)")
-parser.add_argument("-iu", "--iupacname", type=str, help="iupacname column name (" + IUPACNAME + " by default)")
-parser.add_argument("-f", "--molecularformula", type=str,
-                    help="molecularformula column name (" + MOLECULARFORMULA + " by default)")
+parser.add_argument("-i", "--identifier", type=str, help="identifier column name (" + IDENTIFIER + " by default), Text")
+parser.add_argument("-n", "--name", type=str, help="name column name (" + NAME + " by default), Text")
+parser.add_argument("-ink", "--inChIKey", type=str, help="inChIKey column name (" + INCHIKEY + " by default), Text")
+parser.add_argument("-in", "--inChI", type=str, help="inChI column name (" + INCHI + " by default), Text")
+parser.add_argument("-s", "--smiles", type=str, help="smiles column name (" + SMILES + " by default), Text")
+parser.add_argument("-u", "--url", type=str, help="url column name (" + URL + " by default), URL type")
+parser.add_argument("-iu", "--iupacName", type=str, help="iupacName column name (" + IUPAC_NAME + " by default), Text")
+parser.add_argument("-f", "--molecularFormula", type=str,
+                    help="molecularFormula column name (" + MOLECULAR_FORMULA + " by default), Text")
+parser.add_argument("-w", "--molecularWeight", type=str,
+                    help="molecularWeight column name (" + MOLECULAR_WEIGHT + " by default), Mass e.g. 0.01 mg)")
+parser.add_argument("-mw", "--monoisotopicMolecularWeight", type=str,
+                    help="monoisotopicMolecularWeight column name (" + MONOISOTOPIC_MOLECULAR_WEIGHT + " by default), Mass e.g. 0.01 mg")
 parser.add_argument("-d", "--description", type=str,
-                    help="description column name (" + DESCRIPTION + " by default)")
+                    help="description column name (" + DESCRIPTION + " by default), Text")
+parser.add_argument("-dd", "--disambiguatingDescription", type=str,
+                    help="disambiguatingDescription column name (" + DISAMBIGUATING_DESCRIPTION + " by default), Text")
+parser.add_argument("-img", "--image", type=str,
+                    help="image column name (" + IMAGE + " by default), URL")
+parser.add_argument("-at", "--additionalType", type=str,
+                    help="additionalType column name (" + ADDITIONAL_TYPE + " by default), URL")
+parser.add_argument("-an", "--alternateName", type=str,
+                    help="alternateName column name (" + ALTERNATE_NAME + " by default), Text")
+parser.add_argument("-sa", "--sameAs", type=str,
+                    help="sameAs column name (" + SAME_AS + " by default), URL")
 
 parser.add_argument("-c", "--columns",
                     help="Use only columns with renamed names",
@@ -205,22 +304,38 @@ parser.add_argument("-l", "--limit", type=int, help="Maximum number of results")
 args = parser.parse_args()
 
 # replace default column names
-if args.id or args.columns:
-    ID = args.id
+if args.identifier or args.columns:
+    ID = args.identifier
 if args.name or args.columns:
     NAME = args.name
-if args.inchikey or args.columns:
-    INCHIKEY = args.inchikey
-if args.inchi or args.columns:
-    INCHI = args.inchi
+if args.inChIKey or args.columns:
+    INCHIKEY = args.inChIKey
+if args.inChI or args.columns:
+    INCHI = args.inChI
 if args.smiles or args.columns:
     SMILES = args.smiles
 if args.url or args.columns:
     URL = args.url
-if args.iupacname or args.columns:
-    IUPACNAME = args.iupacname
-if args.molecularformula or args.columns:
-    MOLECULARFORMULA = args.molecularformula
+if args.iupacName or args.columns:
+    IUPAC_NAME = args.iupacName
+if args.molecularFormula or args.columns:
+    MOLECULAR_FORMULA = args.molecularFormula
+if args.molecularWeight or args.columns:
+    MOLECULAR_WEIGHT = args.molecularWeight
+if args.monoisotopicMolecularWeight or args.columns:
+    MONOISOTOPIC_MOLECULAR_WEIGHT = args.monoisotopicMolecularWeight
+if args.description or args.columns:
+    DESCRIPTION = args.description
+if args.disambiguatingDescription or args.columns:
+    DISAMBIGUATING_DESCRIPTION = args.disambiguatingDescription
+if args.image or args.columns:
+    IMAGE = args.image
+if args.additionalType or args.columns:
+    ADDITIONAL_TYPE = args.additionalType
+if args.alternateName or args.columns:
+    ALTERNATE_NAME = args.alternateName
+if args.sameAs or args.columns:
+    SAME_AS = args.sameAs
 
 # read csv file and generate outputs
 if args.file:
