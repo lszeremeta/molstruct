36a17ebe75af948eca0895ecaf245adac7e800b6
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2020-10-11 00:21:28 +0200

    Add missing docstring in __init__.py

--- molstruct/__init__.py
+++ molstruct/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
+
+"""Molstruct
+Converts chemical molecule data CSV files to Structured Data formats - JSON-LD, RDFa and Microdata.
+See https://github.com/lszeremeta/molstruct for more information
+"""
+
 __version__ = "2.0.0"
