f741e487d66d53958c97aa7b3f2b04e9a5043ce2
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2020-10-11 00:26:12 +0200

    Add missing blank line in __init__.py

--- molstruct/__init__.py
+++ molstruct/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 """Molstruct
+
 Converts chemical molecule data CSV files to Structured Data formats - JSON-LD, RDFa and Microdata.
-See https://github.com/lszeremeta/molstruct for more information
+See https://github.com/lszeremeta/molstruct for more information.
 """
 
 __version__ = "2.0.0"
